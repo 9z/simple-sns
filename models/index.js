2d2c7f292d7fa9710e94850b820b37bcfe1d0c94
9z <slts0405@gmail.com>
2023-01-10 02:19:11 +0900

    모델 정의 및 DB 연결

--- models/index.js
+++ models/index.js
@@ -1,20 +1,22 @@
 'use strict';
 
+const Sequelize = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+const config = require('../config/config')[env];
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// const User = require('./user');
+// const Post = require('./post');
+// const Hashtag = require('./hashtag');
+
+const db = {};
+const sequelize = new Sequelize(
+  config.database, config.username, config.password, config
+);
+
+db.sequelize = sequelize;
+const basename = path.basename(__filename);
 
 fs
   .readdirSync(__dirname)
@@ -22,8 +24,10 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = require(path.join(__dirname, file));
+    console.log(file, model.name);
     db[model.name] = model;
+    model.initiate(sequelize);
   });
 
 Object.keys(db).forEach(modelName => {
@@ -31,8 +35,16 @@ Object.keys(db).forEach(modelName => {
     db[modelName].associate(db);
   }
 });
+// db.User = User;
+// db.Post = Post;
+// db.Hashtag = Hashtag;
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+// User.initiate(sequelize);
+// Post.initiate(sequelize);
+// Hashtag.initiate(sequelize);
+
+// User.associate(db);
+// Post.associate(db);
+// Hashtag.associate(db);
 
 module.exports = db;
