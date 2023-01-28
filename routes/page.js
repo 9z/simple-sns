370f6ddfba193d002469b01be1dc37ce8d936527
9z <slts0405@gmail.com>
2023-01-28 21:06:10 +0900

    나머지 기능 구현

--- routes/page.js
+++ routes/page.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('../middlewares');
-const { renderProfile, renderJoin, renderMain } = require('../controllers/page');
+const { renderProfile, renderJoin, renderMain, renderHashtag } = require('../controllers/page');
 
 const router = express.Router();
 
 router.use((req, res, next) => {
     res.locals.user = req.user;
-    res.locals.follwerCount = 0;
-    res.locals.followingCount = 0;
-    res.locals.followingIdList = [];
+    console.log('=====================');
+    console.log(req.user?.Followers);
+    console.log(req.user);
+    res.locals.followerCount = req.user?.Followers?.length || 0;
+    res.locals.followingCount = req.user?.Followings?.length || 0;
+    res.locals.followingIdList = req.user?.Followings?.map(f => f.id) || [];
     next();
 });
 
@@ -21,5 +24,7 @@ router.get('/profile', renderProfile);
 router.get('/join', renderJoin);
 
 router.get('/', renderMain);
+
+router.get('/hashtag', renderHashtag);
 
 module.exports = router;
